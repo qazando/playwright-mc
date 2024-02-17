d153e1fbca5b5006c2c0b577aecc077ba8a92b9d
QAzando <qazando@gmail.com>
2024-02-17 09:04:17 -0300

    first commit

---
+++ tests/teste-login.spec.js
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+
+test('teste de login @login', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('herbertao@gmail.com');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+});
+
+test('teste de login 3 @login', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('herbertao@gmail.com');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+});
+
+test('teste de login 1 @login', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('herbertao@gmail.com');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+});
+
+test('teste de login 2 @login', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('herbertao@gmail.com');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+});
