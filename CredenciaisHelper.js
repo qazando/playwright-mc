3a47dddfa11cd1902ef49d75b59d3be9abdcc662
QAzando <qazando@gmail.com>
2024-04-01 13:48:03 -0300

    ajuste novo

---
+++ CredenciaisHelper.js
@@ -0,0 +1,32 @@
+import * as fs from 'fs';
+
+export class CredenciaisHelper {
+  static obterCredenciais() {
+    let email, password;
+
+    // Tenta obter a credencial da variável de ambiente
+    email = process.env.EMAIL;
+    password = process.env.PASSWORD;
+
+    if (!email || !password) {
+      // Tenta obter as credenciais do arquivo credentials.json
+      const filePath = 'credentials.json';
+
+      try {
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        const credentials = JSON.parse(fileContent);
+
+        // Use as credenciais do arquivo JSON
+        email = credentials.email;
+        password = credentials.password;
+      } catch (error) {
+        console.error('Erro ao ler o arquivo credentials.json:', error);
+        // Fornecer valores padrão ou lançar um erro, conforme necessário
+        email = 'seu_email_padrao@example.com';
+        password = 'sua_senha_padrao';
+      }
+    }
+
+    return { email, password };
+  }
+}
--- tests/teste-login.spec.js
+++ tests/teste-login.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { CredenciaisHelper } from '../CredenciaisHelper';
 
 test('teste de login @login', async ({ page }) => {
   await page.goto('https://automationpratice.com.br/');
@@ -7,6 +8,19 @@ test('teste de login @login', async ({ page }) => {
   await page.locator('#user').fill('herbertao@gmail.com');
   await page.locator('#password').click();
   await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await page.getByRole('button', { name: 'OK' }).click();
+});
+
+test('teste de login @login-protegido', async ({ page }) => {
+  const { email, password } = CredenciaisHelper.obterCredenciais();
+
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill(email);
+  await page.locator('#password').click();
+  await page.locator('#password').fill(password);
   await page.getByRole('button', { name: 'login' }).click();
   await page.getByRole('button', { name: 'OK' }).click();
 });
