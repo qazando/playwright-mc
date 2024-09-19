6ce690d69fabb1384f14e92b5450daaa28f8daed
QAzando <65139046+qazando@users.noreply.github.com>
2024-09-19 20:31:56 -0300

    Update register.spec.js

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -12,25 +12,34 @@ test('Cadastro com nome vazio', async ({ page }) => {
 
 test('Cadastro com email vazio @login', async ({ page }) => {
   await page.goto('https://automationpratice.com.br/register');
-  await page.waitForTimeout(25000);
   await page.locator('#user').fill('Teste Qazando')
-  await page.waitForTimeout(25000);
   await page.locator('#password').fill('123456')
-  await page.waitForTimeout(25000);
   await page.locator('#btnRegister').click()
   await expect(page.getByText('O campo e-mail deve ser prenchido corretamente')).toBeVisible();
 });
 
-test('Cadastro com email inválido @cadastro', async ({ page }) => {
-
+test('Cadastro com email inválido @login', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/register');
+  await page.locator('#user').fill('Teste Qazando')
+  await page.locator('#password').fill('123456')
+  await page.locator('#btnRegister').click()
+  await expect(page.getByText('O campo e-mail deve ser prenchido corretamente')).toBeVisible();
 });
 
-test('Cadastro com senha vazia', async ({ page }) => {
-
+test('Cadastro com senha vazia @login', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/register');
+  await page.locator('#user').fill('Teste Qazando')
+  await page.locator('#password').fill('123456')
+  await page.locator('#btnRegister').click()
+  await expect(page.getByText('O campo e-mail deve ser prenchido corretamente')).toBeVisible();
 });
 
-test('Cadastro com senha inválida', async ({ page }) => {
-
+test('Cadastro com senha inválida @login', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/register');
+  await page.locator('#user').fill('Teste Qazando')
+  await page.locator('#password').fill('123456')
+  await page.locator('#btnRegister').click()
+  await expect(page.getByText('O campo e-mail deve ser prenchido corretamente')).toBeVisible();
 });
 
 test('Cadastro com formulário vazio', async ({ page }) => {
