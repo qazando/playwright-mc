d8ceae8af24c0a07dd39572b59a2ae705469c3d4
QAzando <65139046+qazando@users.noreply.github.com>
2024-09-19 20:26:03 -0300

    Update register.spec.js

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -5,18 +5,18 @@ test('Cadastro com nome vazio', async ({ page }) => {
   await page.goto('https://automationpratice.com.br/register');
   await page.locator('#email').fill('teste@teste.com')
   await page.locator('#password').fill('123456')
-  await page.waitForTimeout(60000);
+  await page.waitForTimeout(25000);
   await page.locator('#btnRegister').click()
   await expect(page.getByText('O campo nome deve ser')).toBeVisible();
 });
 
 test('Cadastro com email vazio @login', async ({ page }) => {
   await page.goto('https://automationpratice.com.br/register');
-  await page.waitForTimeout(60000);
+  await page.waitForTimeout(25000);
   await page.locator('#user').fill('Teste Qazando')
-  await page.waitForTimeout(60000);
+  await page.waitForTimeout(25000);
   await page.locator('#password').fill('123456')
-  await page.waitForTimeout(60000);
+  await page.waitForTimeout(25000);
   await page.locator('#btnRegister').click()
   await expect(page.getByText('O campo e-mail deve ser prenchido corretamente')).toBeVisible();
 });
